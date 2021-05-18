593210c9cf2b6a6c9557d43c287d10361f3c7136
N1coc4colA <50690360+N1coc4colA@users.noreply.github.com>
2021-05-17 22:15:31 -0930

    Fix version and add a way to get the version
    Use a better versioning, and put what have to be.

--- libda_global.h
+++ libda_global.h
@@ -9,9 +9,19 @@
 #include "class_decl.h"
 #include "macros.h"
 
+//The versioning: <major>.<sub>.<minor>.<revision>.<status>
+//The different status that are possible:
+//"a" -> Alpha
+//"b" -> Beta
+//"p" -> Pre-release
+//"r" -> Release
+//Usually p and r are exposed on Github, a and b stays local.
+
 #define LDA_MAJOR 1
-#define LDA_MINOR 0
+#define LDA_SUB 0
+#define LDA_MINOR 5
 #define LDA_REVISION 1
+#define LDA_STATUS #r
 
 #if defined(LIBDA_GLOBAL_H)
     #define LIBDA_SHARED_EXPORT Q_DECL_EXPORT
@@ -19,7 +29,13 @@
     #define LDA_END_NAMESPACE }}
     #define LDA_USE_NAMESPACE using namespace Dtk::Addons;
 #else
-#  define LIBDADDONS_SHARED_EXPORT Q_DECL_IMPORT
+    #define LIBDADDONS_SHARED_EXPORT Q_DECL_IMPORT
 #endif
+
+static inline int get_lda_major() {return LDA_MAJOR;}
+static inline int get_lda_sub() {return LDA_SUB;}
+static inline int get_lda_minor() {return LDA_MINOR;}
+static inline int get_lda_revision() {return LDA_REVISION;}
+static inline const char* get_lda_status() {return LDA_STATUS;}
 
 #endif // LIBDA_GLOBAL_H
